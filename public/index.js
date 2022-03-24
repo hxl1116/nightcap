a49c94eccfcd1d1761282d0f065c4ee7cdaa2257
Henry Larson <hxl1116@rit.edu>
2022-03-24 12:09:20 -0400

    Added CSS; stubbed out handler func

---
+++ public/index.js
@@ -0,0 +1,19 @@
+const clubInfo = []
+
+let clubNamesSelect = document.querySelector('#club-names-select')
+let clubNameText = document.querySelector('#club-name')
+let clubLocText = document.querySelector('#club-loc')
+let clubCapText = document.querySelector('#club-cap')
+let clubThemeText = document.querySelector('#club-theme')
+
+let infoBox = document.querySelector('#club-info')
+
+const updateInfoBox = () => {
+
+}
+
+const handleSelect = () => {
+
+}
+
+clubNamesSelect.addEventListener('change', handleSelect)
