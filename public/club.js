18059a962458ac3945f1a8fc5371b5dbf88be1c4
Henry Larson <hxl1116@rit.edu>
2022-03-30 23:44:59 -0400

    Added CSS; implemented class swapping depending on club volume

--- public/club.js
+++ public/club.js
@@ -14,15 +14,15 @@ $(document).ready(() => {
 // Club capacity levels
 const CAPACITIES = {
     normal: {
-        color: 'green',
+        color: '#94d2bd',
         message: 'Welcome!'
     },
     caution: {
-        color: 'red',
+        color: '#ca6702',
         message: 'Warn the bouncers...'
     },
     restricted: {
-        color: 'red',
+        color: '#ae2012',
         message: 'No one allowed in!'
     }
 }
@@ -31,28 +31,28 @@ const CAPACITIES = {
 const CLUBS = [
     {
         id: 1,
-        name: 'clubArcane',
+        name: 'club-arcane',
         text: 'Club Arcane',
         capacity: 100,
         threshold: 70
     },
     {
         id: 2,
-        name: 'clubUnderground',
+        name: 'club-underground',
         text: 'Club Underground',
         capacity: 50,
         threshold: 30
     },
     {
         id: 3,
-        name: 'clubSoda',
+        name: 'club-soda',
         text: 'Club Soda',
         capacity: 20,
         threshold: 12
     },
     {
         id: 4,
-        name: 'studio52',
+        name: 'studio-52',
         text: 'Studio 52',
         capacity: 52,
         threshold: 32
@@ -74,10 +74,11 @@ const init = () => {
         // Insert club display elements
         $('#clubs-list').append(
             $(`<div class="club-display" id="${club.name}-display">`).append(
-                $('<div class="club-info">').append(
-                    $('<p class="club-name">').text(club.text),
-                    $('<p class="club-msg">')
-                ),
+                $('<div class="club-info normal">')
+                    .append(
+                        $('<p class="club-name">').text(club.text),
+                        $('<p class="club-msg">')
+                    ),
                 $('<p class="cap-count">').text(volumes[club.name])
             )
         )
@@ -85,51 +86,64 @@ const init = () => {
         // Insert club radio options
         $('#club-opts').append(
             $('<label>')
-                .text(club.name),
-            $(`<input id="${club.name}-opt" name="club-opt" type="radio">`).click(() => {
-                activeClub = club.name
-            })
+                .text(club.text)
+                .prepend(
+                    $(`<input id="${club.name}-opt" name="club-opt" type="radio">`).click(() => {
+                        activeClub = club.name
+                    })
+                ),
         )
     }
 }
 
 const handleStatus = ({action}) => {
     let club = CLUBS.filter(club => club.name === activeClub).pop()
+    let disp = $(`#${club.name}-display>div`)
     let vol = volumes[activeClub]
     let msg = null
+
+    disp.removeClass('normal')
+    disp.removeClass('caution')
+    disp.removeClass('restricted')
 
     switch (action) {
         case 'increment':
             // If club volume is between threshold and max capacity
             if (vol >= club.threshold && vol < club.capacity) {
                 volumes[activeClub] += 1
+                disp.addClass('caution')
                 msg = CAPACITIES.caution
                 break
             }
             // If club volume is less than threshold
             else if (vol < club.threshold) {
                 volumes[activeClub] += 1
+                disp.addClass('normal')
                 msg = CAPACITIES.normal
                 break
             }
             // If club volume is at capacity
             else {
+                disp.addClass('restricted')
                 msg = CAPACITIES.restricted
                 break
             }
         case 'decrement':
             // If club volume is between threshold and max capacity
             if (vol >= club.threshold && vol < club.capacity) {
+                disp.addClass('caution')
                 msg = CAPACITIES.caution
                 break
             }
             // If club volume is less than threshold
             else if (vol < club.threshold) {
+                disp.addClass('normal')
                 msg = CAPACITIES.normal
                 break
             }
             // If club volume is at capacity
             else {
+                disp.addClass('restricted')
                 msg = CAPACITIES.restricted
                 break
             }
