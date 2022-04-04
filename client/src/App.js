fe7ded5f0799a272ca827ea8b8d4a849ce3f63a6
Henry Larson <hxl1116@rit.edu>
2022-04-04 13:00:04 -0400

    Recreated React app

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi Mom!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hi Mom!</h1>
+        </div>
+    );
 }
 
 export default App;
