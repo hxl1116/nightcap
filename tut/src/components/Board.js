f853592668941598da9fe1153c1ae0919330383c
Henry Larson <hxl1116@rit.edu>
2022-04-04 21:00:06 -0400

    Added constructor to `Square`; made `Square` interactive

--- tut/src/components/Board.js
+++ tut/src/components/Board.js
@@ -2,8 +2,15 @@ import React from "react";
 import Square from "./Square";
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square/>;
+        return <Square value={this.state.squares[i]}/>
     }
 
     render() {
