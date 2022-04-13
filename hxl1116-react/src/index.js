5f408fe84713d4a02157a2936fcc25670e36ca41
Henry Larson <hxl1116@rit.edu>
2022-04-13 11:05:13 -0400

    Added `reactstrap`, `bootstrap` to dependencies

--- hxl1116-react/src/index.js
+++ hxl1116-react/src/index.js
@@ -3,6 +3,9 @@ import {createRoot} from 'react-dom/client';
 
 import App from './App';
 
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = createRoot(document.getElementById('root'))
 
 root.render(
