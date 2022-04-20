cb6abbd93c9da07aac5b0f2acde63f2256d3cd57
Henry Larson <hxl1116@rit.edu>
2022-04-20 18:25:53 -0400

    Added club-related db access func

--- server/db/club.py
+++ server/db/club.py
@@ -1,21 +1,38 @@
-from utils import fetch_many
+from psycopg2.sql import SQL, Identifier
+
+from utils import fetch_many, fetch_one, commit
 
 
 def get_all_clubs():
+    """ Get all clubs """
     return fetch_many("SELECT * FROM club")
 
 
-def get_club():
-    pass
+def get_club(name):
+    """ Get one club by name """
+    return fetch_one("SELECT * FROM club WHERE name = %s", (name,))
 
 
-def insert_club():
-    pass
+def insert_club(**kwargs):
+    """ Insert a new club """
+    commit("""
+        INSERT INTO club (name, genre, city, capacity, threshold) VALUES (%s)
+    """, (tuple(kwargs.values())))
 
 
-def update_club():
-    pass
+def update_club(id, **kwargs):
+    """ Update a club's info """
+    query = SQL("""
+        UPDATE club SET ({}) = %s WHERE id = %s
+    """).format(
+        SQL(', ').join(map(Identifier, list(kwargs.keys())))
+    )
+
+    commit(query, (tuple(kwargs.values()), id))
 
 
-def delete_club():
-    pass
+def delete_club(id):
+    """ Delete a club """
+    commit("""
+        DELETE FROM club WHERE id = %s
+    """, (id,))
